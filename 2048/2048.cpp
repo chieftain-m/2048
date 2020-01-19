77571ffdd7f76efc03257b08b7af97291d4e1697
胡骏 <hu_1999216@163.com>
2020-01-19 10:13:23 +0800

    init

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -1,1 +1,41 @@
-﻿
+﻿#include"pch.h"
+#include"2048.h"
+#include<iostream>
+#include"easyx.h"
+#include<conio.h>
+using namespace std;
+
+void Init(int map[][MAP_COL], int score) {
+	int x, y;
+	score = 0;
+	memset(map, 0, sizeof(map[MAP_ROW][MAP_COL]));
+	for (int i = 0; i < 2;) {
+		x = rand() % 4; 
+		y = rand() % 4;
+		if (map[x][y] == 0) {
+			if (rand() % 7 == 0) {
+				map[x][y] = 4;
+			}
+			else {
+				map[x][y] = 2;
+			}
+			i++;
+		}
+	}
+}
+void ReadData(int map[][MAP_COL], int score)
+{
+	FILE*fp = fopen("D:\\2048.txt", "rt");
+	if (fp == NULL) return;
+	fread(map, sizeof(int)*MAP_ROW*MAP_COL, 1, fp);
+	fread((void*)score, sizeof(int), 1, fp);
+	fread((void*)MaxScore, sizeof(int), 1, fp);
+	fclose(fp);
+}
+int main() {
+	int map[MAP_ROW][MAP_COL];
+	int score = 0;
+	initgraph(MAP_ROW * PIXEL + 100, MAP_COL * PIXEL + 100);
+	Init(map, score);			//初始化地图数据
+	ReadData(map, score);		//读取数据
+}
---
+++ 2048/2048.h
@@ -0,0 +1,7 @@
+#pragma once
+const int MAP_ROW = 4;
+const int MAP_COL = 4;
+const int PIXEL = 100;
+int MaxScore = 0;
+void Init(int map[][MAP_COL], int score);		//≥ı ºªØ”Œœ∑
+void ReadData(int map[][MAP_COL], int score);
